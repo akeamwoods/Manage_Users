af81e091a8ccc4f4610058aa371f9da19e62cd27
akeamwoods <a19941@hotmail.co.uk>
2020-10-02 18:36:27 +0100

    Updates

---
+++ precache-manifest.48c67b8f1d7029c26c3f98ea31eabd9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317f325e210621f724c32eb32650539a",
+    "url": "/Manage_Users/index.html"
+  },
+  {
+    "revision": "191f05df21db489d85fa",
+    "url": "/Manage_Users/static/css/2.6db69bf1.chunk.css"
+  },
+  {
+    "revision": "d02688a585b7a5dd2426",
+    "url": "/Manage_Users/static/css/main.94ba9d5e.chunk.css"
+  },
+  {
+    "revision": "191f05df21db489d85fa",
+    "url": "/Manage_Users/static/js/2.7e87e61a.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/Manage_Users/static/js/2.7e87e61a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d02688a585b7a5dd2426",
+    "url": "/Manage_Users/static/js/main.00abb163.chunk.js"
+  },
+  {
+    "revision": "40df7dc91e14efc372d3",
+    "url": "/Manage_Users/static/js/runtime-main.f2ca40cd.js"
+  }
+]);
